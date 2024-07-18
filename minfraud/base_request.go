b8ee1ce2b8d901cc9109b87e75d9ef9b7eecf4ed
Tim-Blyth-SiteHost <75048324+Tim-Blyth-SiteHost@users.noreply.github.com>
2024-07-18 13:33:29 +1200

    Update base_request.go to omit empty fields (#1)

--- minfraud/base_request.go
+++ minfraud/base_request.go
@@ -19,42 +19,42 @@ type BaseRequest struct {
 }
 
 type AccountData struct {
-	UserID      string `json:"user_id"`
-	UserNameMD5 string `json:"username_md5"`
+	UserID      string `json:"user_id,omitempty"`
+	UserNameMD5 string `json:"username_md5,omitempty"`
 }
 
 type BillingData struct {
-	Address          string `json:"address"`
-	Address2         string `json:"address_2"`
-	City             string `json:"city"`
-	Company          string `json:"company"`
-	Country          string `json:"country"`
-	FirstName        string `json:"first_name"`
-	LastName         string `json:"last_name"`
-	PhoneCountryCode string `json:"phone_country_code"`
-	PhoneNumber      string `json:"phone_number"`
-	Postal           string `json:"postal"`
-	Region           string `json:"region"`
+	Address          string `json:"address,omitempty"`
+	Address2         string `json:"address_2,omitempty"`
+	City             string `json:"city,omitempty"`
+	Company          string `json:"company,omitempty"`
+	Country          string `json:"country,omitempty"`
+	FirstName        string `json:"first_name,omitempty"`
+	LastName         string `json:"last_name,omitempty"`
+	PhoneCountryCode string `json:"phone_country_code,omitempty"`
+	PhoneNumber      string `json:"phone_number,omitempty"`
+	Postal           string `json:"postal,omitempty"`
+	Region           string `json:"region,omitempty"`
 }
 
 type CreditCardData struct {
-	AVSResult             string `json:"avs_result"`
-	BankName              string `json:"bank_name"`
-	BankPhoneCountryCode  string `json:"bank_phone_country_code"`
-	BankPhoneNumber       string `json:"bank_phone_number"`
-	CVVResult             string `json:"cvv_result"`
-	IssuerIDNumber        string `json:"issuer_id_number"`
-	Last4Digits           string `json:"last_4_digits"`
-	Token                 string `json:"token"`
-	Was3DSecureSuccessful bool   `json:"was_3d_secure_successful"`
+	AVSResult             string `json:"avs_result,omitempty"`
+	BankName              string `json:"bank_name,omitempty"`
+	BankPhoneCountryCode  string `json:"bank_phone_country_code,omitempty"`
+	BankPhoneNumber       string `json:"bank_phone_number,omitempty"`
+	CVVResult             string `json:"cvv_result,omitempty"`
+	IssuerIDNumber        string `json:"issuer_id_number,omitempty"`
+	Last4Digits           string `json:"last_4_digits,omitempty"`
+	Token                 string `json:"token,omitempty"`
+	Was3DSecureSuccessful *bool   `json:"was_3d_secure_successful,omitempty"`
 }
 
 type DeviceData struct {
-	AcceptLanguage string  `json:"accept_language"`
-	IPAddress      string  `json:"ip_address"`
-	SessionAge     float64 `json:"session_age"`
-	SessionID      string  `json:"session_id"`
-	UserAgent      string  `json:"user_agent"`
+	AcceptLanguage string  `json:"accept_language,omitempty"`
+	IPAddress      string  `json:"ip_address,omitempty"`
+	SessionAge     float64 `json:"session_age,omitempty"`
+	SessionID      string  `json:"session_id,omitempty"`
+	UserAgent      string  `json:"user_agent,omitempty"`
 }
 
 type EmailData struct {
@@ -63,47 +63,47 @@ type EmailData struct {
 }
 
 type EventData struct {
-	ShopID        string `json:"shop_id"`
-	Time          string `json:"time"`
-	TransactionID string `json:"transaction_id"`
-	Type          string `json:"type"`
+	ShopID        string `json:"shop_id,omitempty"`
+	Time          string `json:"time,omitempty"`
+	TransactionID string `json:"transaction_id,omitempty"`
+	Type          string `json:"type,omitempty"`
 }
 
 type OrderData struct {
-	AffiliateID    string  `json:"affiliateID"`
-	Amount         float64 `json:"amount"`
-	Currency       string  `json:"currency"`
-	DiscountCode   string  `json:"discount_code"`
-	HasGiftMessage bool    `json:"has_gift_message"`
-	IsGift         bool    `json:"is_gift"`
-	ReferrerURI    string  `json:"referrer_uri"`
-	SubaffiliateID string  `json:"subaffiliate_id"`
+	AffiliateID    string  `json:"affiliateID,omitempty"`
+	Amount         *float64 `json:"amount,omitempty"`
+	Currency       string  `json:"currency,omitempty"`
+	DiscountCode   string  `json:"discount_code,omitempty"`
+	HasGiftMessage *bool    `json:"has_gift_message,omitempty"`
+	IsGift         *bool    `json:"is_gift,omitempty"`
+	ReferrerURI    string  `json:"referrer_uri,omitempty"`
+	SubaffiliateID string  `json:"subaffiliate_id,omitempty"`
 }
 
 type PaymentData struct {
-	DeclineCode   string `json:"decline_code"`
-	Processor     string `json:"processor"`
-	WasAuthorized bool   `json:"was_authorized"`
+	DeclineCode   string `json:"decline_code,omitempty"`
+	Processor     string `json:"processor,omitempty"`
+	WasAuthorized *bool   `json:"was_authorized,omitempty"`
 }
 
 type ShippingData struct {
-	Address          string `json:"address"`
-	Address2         string `json:"address_2"`
-	City             string `json:"city"`
-	Company          string `json:"company"`
-	Country          string `json:"country"`
-	DeliverySpeed    string `json:"delivery_speed"`
-	FirstName        string `json:"first_name"`
-	LastName         string `json:"last_name"`
-	PhoneCountryCode string `json:"phone_country_code"`
-	PhoneNumber      string `json:"phone_number"`
-	Postal           string `json:"postal"`
-	Region           string `json:"region"`
+	Address          string `json:"address,omitempty"`
+	Address2         string `json:"address_2,omitempty"`
+	City             string `json:"city,omitempty"`
+	Company          string `json:"company,omitempty"`
+	Country          string `json:"country,omitempty"`
+	DeliverySpeed    string `json:"delivery_speed,omitempty"`
+	FirstName        string `json:"first_name,omitempty"`
+	LastName         string `json:"last_name,omitempty"`
+	PhoneCountryCode string `json:"phone_country_code,omitempty"`
+	PhoneNumber      string `json:"phone_number,omitempty"`
+	Postal           string `json:"postal,omitempty"`
+	Region           string `json:"region,omitempty"`
 }
 
 type ShoppingCartData struct {
-	Category string  `json:"category"`
-	ItemID   string  `json:"item_id"`
-	Price    float64 `json:"price"`
-	Quantity int64   `json:"quantity"`
+	Category string  `json:"category,omitempty"`
+	ItemID   string  `json:"item_id,omitempty"`
+	Price    *float64 `json:"price,omitempty"`
+	Quantity *int64   `json:"quantity,omitempty"`
 }
